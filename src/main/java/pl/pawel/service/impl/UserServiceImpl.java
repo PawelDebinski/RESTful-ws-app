a04833405a5d9e69d2adde688dc6f0bcb53de551
Pawel Debinski <spokojasne@interia.eu>
2018-09-22 13:15:46 +0200

    autowired user repo into service, closes #10

--- src/main/java/pl/pawel/service/impl/UserServiceImpl.java
+++ src/main/java/pl/pawel/service/impl/UserServiceImpl.java
@@ -1,15 +1,32 @@
 package pl.pawel.service.impl;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.pawel.io.entity.UserEntity;
+import pl.pawel.io.repository.UserRepository;
 import pl.pawel.service.UserService;
 import pl.pawel.shared.dto.UserDto;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto user) {
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
 
-        return null;
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("testUserId");
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails, returnValue);
+
+        return returnValue;
     }
 }
