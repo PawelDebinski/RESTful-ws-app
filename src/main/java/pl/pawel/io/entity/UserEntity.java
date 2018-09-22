11bc29f1408b3b28b5fe76b5db8d16fc61afb279
Pawel Debinski <spokojasne@interia.eu>
2018-09-22 11:29:07 +0200

    added user entity, closes #8

--- src/main/java/pl/pawel/io/entity/UserEntity.java
+++ src/main/java/pl/pawel/io/entity/UserEntity.java
@@ -1,25 +1,45 @@
-package pl.pawel.shared.dto;
+package pl.pawel.io.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.io.Serializable;
 
-public class UserDto implements Serializable {
+@Entity(name = "users")
+public class UserEntity implements Serializable {
 
-    private static final long serialVersionUID = 4865903039190150223L;
+    private static final long serialVersionUID = 5096462602451344775L;
+
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
     private String userId;
+
+    @Column(nullable = false, length = 50)
     private String firstName;
+
+    @Column(nullable = false, length = 50)
     private String lastName;
+
+    @Column(nullable = false, length = 120)
     private String email;
-    private String password;
+
+    @Column(nullable = false)
     private String encryptedPassword;
+
     private String emailVerificationToken;
+
+    @Column(nullable = false, columnDefinition = "boolean default false")
     private Boolean emailVerificationStatus;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -53,14 +73,6 @@ public class UserDto implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getEncryptedPassword() {
