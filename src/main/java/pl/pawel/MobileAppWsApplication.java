57005ec343fad7b0505fec6738fb5e42bcbef745
Pawel Debinski <spokojasne@interia.eu>
2018-09-26 09:16:17 +0200

    added password encryption, closes #15

--- src/main/java/pl/pawel/MobileAppWsApplication.java
+++ src/main/java/pl/pawel/MobileAppWsApplication.java
@@ -2,11 +2,18 @@ package pl.pawel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MobileAppWsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MobileAppWsApplication.class, args);
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
